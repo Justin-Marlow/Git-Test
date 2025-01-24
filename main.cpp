45dfade0dc482c1613dc988e2b06096194fbb791
Justin Marlow <jusdmarl@ut.utm.edu>
2025-01-24 14:55:25 -0600

    Added product funtion to calculate product from 1 to n.

--- main.cpp
+++ main.cpp
@@ -10,10 +10,20 @@ int sum(int n) {
   return total;
 }
 
+int product(int n) {
+  int total = 1;
+  for (int i = 1; i <=n; ++i) {
+    total *= i;
+  }
+  return total;
+}
+
+
 int main() {
   int n;
   cout << "Enter a number: ";
   cin >> n;
   cout << "The sum from 1 to " << n << " is " << sum(n) << "." << endl;
+  cout << "The product from 1 to " << n << " is " << product(n) << "." << endl;
   return 0;
 }
